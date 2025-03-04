b868412f69ac797e0d003088588718d1060823a2
addy <addyjaime@gmail.com>
2025-03-04 06:08:39 -0500

    this version is working

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,28 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        "module-resolver", //allow to use short routes inatead of long routes
+        'module-resolver',
         {
-          root: ["./src"],
+          root: ['./src'],
           alias: {
-            "@api": "./src/api",
-            "@assets": "./src/assets",
-            "@components": "./src/components",
-            "@constants": "./src/Constants",
-            "@contexts": "./src/contexts",
-            "@hooks": "./src/hooks",
-            "@navigation": "./src/navigation",
-            "@routes": "./src/Routes",
-            "@screens": "./src/screens",
-            "@styles": "./src/styles",
-            "@utils": "./src/utils",
+            '@api': './src/api',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@constants': './src/constants',
+            '@contexts': './src/contexts',
+            '@hooks': './src/hooks',
+            '@navigation': './src/navigation',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@styles': './src/styles',
+            '@utils': './src/utils',
           },
         },
       ],
-      ["module:react-native-dotenv"],
+      ['module:react-native-dotenv'],
     ],
   };
 };
