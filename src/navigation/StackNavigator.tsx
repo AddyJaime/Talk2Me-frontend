140b48397479c0bd06d5dd196e87d980fc8cd0f1
addy <addyjaime@gmail.com>
2025-03-11 06:55:09 -0400

    finishing login from

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -4,19 +4,19 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // Screens
-// import SplashScreen from '@screens/SplashScreen';
+import SplashScreen from '@screens/SplashScreen/SplashScreen';
 import LoginScreen from '@screens/LoginScreen/LoginScreeen';
 
 const Stack = createStackNavigator();
 
 const StackNavigator: React.FC = () => {
   return (
-    <Stack.Navigator>
-      {/* <Stack.Screen
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
         name="Splash"
         component={SplashScreen}
         options={{ headerShown: false }}
-      /> */}
+      />
       <Stack.Screen
         name="Login"
         component={LoginScreen}
--- src/screens/LoginScreen/LoginScreeen.tsx
+++ src/screens/LoginScreen/LoginScreeen.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { View, TextInput, Image, TouchableOpacity, Text } from 'react-native';
+import {
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Text,
+  StatusBar,
+} from 'react-native';
 import { Controller, useForm } from 'react-hook-form';
 import styles from '@screens/LoginScreen/styles';
 import talk2meBgOff from '@assets/images/Talk2me-Background-Off.png';
+import backgroundImage from '@assets/images/background.png';
+import lightImage from '@assets/images/light.png';
 
 const LoginScreen: React.FC = () => {
   const { control, handleSubmit } = useForm();
@@ -13,6 +22,9 @@ const LoginScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
+      <Image source={backgroundImage} style={styles.backgroundImage} />
+      <Image source={lightImage} style={styles.lightImage} />
+      <Image source={lightImage} style={styles.lightImageTwo} />
       <Image source={talk2meBgOff} style={styles.logo} />
       <View style={styles.form}>
         <Controller
@@ -28,12 +40,26 @@ const LoginScreen: React.FC = () => {
             />
           )}
         />
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              onChange={field.onChange}
+              value={field.value}
+            />
+          )}
+        />
+
         <TouchableOpacity
           style={styles.button}
           onPress={handleSubmit(onSubmit)}
         >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
+        <StatusBar backgroundColor="black" />
       </View>
     </View>
   );
