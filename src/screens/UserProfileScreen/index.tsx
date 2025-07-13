bc2fc107a594499491a43bee3641f3bfdd412696
addy <addyjaime@gmail.com>
2025-07-13 19:16:01 -0400

    ..

--- src/screens/UserProfileScreen/index.tsx
+++ src/screens/UserProfileScreen/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Modal, TextInput } from 'react-native';
 import styles from './styles';
 import UserAvatar from 'react-native-user-avatar';
 
 export const UserProfile: React.FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [name, setName] = useState('addy jaime');
+
+  const opennModal = () => {
+    setIsModalVisible(true);
+  };
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.statusText}>User Profile</Text>
-      <UserAvatar style={styles.avatar} size={100} />
+      <Text style={styles.statusText}>Profile</Text>
+      <View style={styles.innerContainer}>
+        <UserAvatar style={styles.avatar} size={100} />
+        <Text>Name</Text>
+        <TouchableOpacity onPress={opennModal}>
+          <Text style={styles.name}>Addy Jaime</Text>
+        </TouchableOpacity>
+      </View>
+      <Modal visible={isModalVisible} style={styles.modalContainer}>
+        <TouchableOpacity onPress={closeModal}>
+          <Text style={styles.cancelbutton}>Cancel</Text>
+          <Text style={styles.saveButton}>Save</Text>
+        </TouchableOpacity>
+        <TextInput style={styles.name}>{name}</TextInput>
+      </Modal>
     </View>
   );
 };
--- src/screens/UserProfileScreen/styles.ts
+++ src/screens/UserProfileScreen/styles.ts
@@ -5,15 +5,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f0f0f0',
   },
+  innerContainer: {
+    flex: 1,
+  },
   statusText: {
     marginVertical: 80,
     marginHorizontal: 18,
     fontSize: 35,
-
   },
   avatar: {
-
     width: 100
+  },
+  name: {
+    backgroundColor: "gray",
+    padding: 10,
+    margin: 20,
+  },
+  modalContainer: {
+    backgroundColor: "black"
+  },
+  cancelbutton: {
+    margin: 80
+  },
+  saveButton: {
+    margin: 40
   }
 });
 
